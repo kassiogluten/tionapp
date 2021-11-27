aa551cdba65510e0853ea7547a1477faea937870
Kassio <kassiogluten@gmail.com>
2021-11-27 19:56:37 -0300

    hero img replace

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,11 @@
-import {
-  Flex,
-  Text,
-  VStack,
-} from "@chakra-ui/react";
+import { Flex, Text, VStack, Link, Image } from "@chakra-ui/react";
 import { Botao } from "./Botao";
 
-import Image from 'next/image'
+// import Image from "next/image";
 
 export function Hero() {
   return (
-    <Flex  as="section" justify="center" align="center" w="100%">
+    <Flex as="section" justify="center" align="center" w="100%">
       <Flex
         p="5rem 1rem"
         align="center"
@@ -18,12 +14,26 @@ export function Hero() {
       >
         <VStack spacing={0} align="start" flex={1}>
           <Text fontSize={24}>O Tion cobra tarifa</Text>
-          <Text lineHeight={0.7} bgGradient="linear(to-r, pessego, gradient2)" bgClip="text" fontWeight="bold" fontSize={{base:120, md:200}}>zero</Text>
-          <Text fontSize={{base:45, md:75}}>por corrida</Text>
-          <Text py={8}>Você paga somente uma taxa de R$ 00,00 para uso do aplicativo mensalmente.</Text>
-          <a href="#cadastre"><Botao url="#cadastre" text="Seja um motorista" w="full" /></a>
+          <Text
+            lineHeight={0.7}
+            bgGradient="linear(to-r, pessego, gradient2)"
+            bgClip="text"
+            fontWeight="bold"
+            fontSize={{ base: 120, md: 200 }}
+          >
+            zero
+          </Text>
+          <Text fontSize={{ base: 45, md: 75 }}>por corrida</Text>
+          <Text py={8}>
+            Você paga somente uma taxa de R$ 00,00 para uso do aplicativo
+            mensalmente.
+          </Text>
+          <Botao as="a" url="#cadastre" text="Seja um motorista" w="full" />
         </VStack>
-          <Image quality={100} width={678} height={773} src="/tion1.png" alt="Prévia do app em smartphones"></Image>
+        <Image transform={{base:"translateX(-10%)",md:"translateX(0)"}}
+          src="/tion1.png"
+          alt="Prévia do app em smartphones"
+        ></Image>
       </Flex>
     </Flex>
   );
